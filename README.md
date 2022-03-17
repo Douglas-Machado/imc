507bafda5c9cc3482a11543400da13c7936eee06
Douglas C Machado <douglasclaudino10.dc@gmail.com>
2022-03-17 19:05:04 -0300

    v1

---
+++ README.md
@@ -0,0 +1,11 @@
+# Cáculo IMC
+
+## Projeto desenvolvido para estudo JS para receber dados de peso e altura e calcular IMC do usuário
+
+### Usando a calculadora de IMC
+
+* Após clonar o repositório, basta abrir o arquivo index.html e insrir os dados necessários para o cálculo IMC
+
+* Peso(Kg)
+* Altura(M)
+
---
+++ assets/js/main.js
@@ -0,0 +1,36 @@
+const submit = document.getElementById("submit");
+var result = document.getElementById("result");
+
+function getImc (weight, height){
+  let value = (weight.value / Math.pow(height.value, 2));
+  return(value)
+}
+
+function getImcLevel (){
+  event.preventDefault()
+  const weight = document.getElementById("input-1");
+  const height = document.getElementById("input-2");
+  imc = getImc(weight, height);
+
+  
+  let answer
+  console.log(imc)
+  if(imc < 18.5){
+    answer = "Abaixo do peso"
+  }else if(imc >= 18.5){
+    answer = "Peso normal"
+  }else if(imc >= 24.9){
+    answer = "Sobrepeso"
+  }else if(imc >= 29.9){
+    answer = "Obesidade grau 1"
+  }else if(imc >= 34.9 && imc < 40){
+    answer = "Obesidade grau 2"
+  }else{
+    answer = "Obesidade grau 3"
+  }
+
+  result.style.display = "block"
+  document.getElementById("result-text").innerHTML = `Seu IMC é ${imc.toFixed(2)} (${answer})`;
+}
+
+submit.addEventListener("click", getImcLevel)
