f1c54f7420a0750fb1567df17e924c6ae6421ba0
Douglas C Machado <douglasclaudino10.dc@gmail.com>
2022-04-15 16:06:16 -0300

    v1.2

--- assets/js/main.js
+++ assets/js/main.js
@@ -3,8 +3,8 @@ var result = document.getElementById("result");
 
 function getImc (weight, height){
   let value = (weight.value / Math.pow(height.value, 2));
-  if(isNaN(value)){
-    alert("Insira um número")
+  if(isNaN(value) || value == 'Infinity'){
+    alert("Insira um valor válido")
     location.reload();
   }
   return(value)
@@ -19,19 +19,17 @@ function getImcLevel (){
   
   let answer
   console.log(imc)
-  if(imc < 18.5){
-    answer = "Abaixo do peso"
-  }else if(imc >= 18.5){
+  console.log(typeof(imc))
+  if(imc < 18.49){answer = "Abaixo do peso"
+  } else if(imc < 24.9){
     answer = "Peso normal"
-  }else if(imc >= 24.9){
+  } else if(imc < 29.9){
     answer = "Sobrepeso"
-  }else if(imc >= 29.9){
+  } else if(imc < 34.9){
     answer = "Obesidade grau 1"
-  }else if(imc >= 34.9 && imc < 40){
+  } else if(imc < 40){
     answer = "Obesidade grau 2"
-  }else{
-    answer = "Obesidade grau 3"
-  }
+  } else {answer = "Obesidade grau 3"}
 
   result.style.display = "block"
   document.getElementById("result-text").innerHTML = `Seu IMC é ${imc.toFixed(2)} (${answer})`;
